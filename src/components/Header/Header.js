d38b68147faae88589da4af0c4c36283197345a5
pranav madhu <pranavmadhu73@gmail.com>
2022-09-24 23:35:37 +0530

    added header and bg

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 
-function Header() {
-  return <header></header>;
+function Header({ anchorlinks }) {
+  console.log(anchorlinks);
+  return (
+    <header className="header">
+      <img src="" alt="" className="header_logo" />
+      <nav className="header_navbar">
+        <ul>
+          {anchorlinks.map((al) => (
+            <li>
+              <a href={"#" + al}>{al}</a>
+            </li>
+          ))}
+          <li className="header_register_button">
+            <a href="">REGISTER</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import "./Home.css";
+import mousie from "../../assets/images/mousie.png";
 
 function Home() {
   return (
-    <div className="Home_wrapper">
-      <Header />
+    <div className="home_wrapper">
+      <Header anchorlinks={["home", "events", "about", "gallery", "contact"]} />
+      <img src={mousie} />
+      <div className="home_hero_wrapper"></div>
     </div>
   );
 }
