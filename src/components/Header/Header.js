00ff7faaf0497717c645db2b96f2e6101ba3f2d4
pranav madhu <pranavmadhu73@gmail.com>
2022-09-24 20:03:24 +0530

    added basic header footer component folders to the project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  return <header></header>;
+}
+
+export default Header;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home_wrapper">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
