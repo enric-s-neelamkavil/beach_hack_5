c3468747b9ad89028c38f7d5b946d4df78fa63ae
pranav madhu <pranavmadhu73@gmail.com>
2022-10-02 21:50:42 +0530

    changed the design of sponsor cards

--- src/components/Sponsors/Sponsorcard/Sponsorcard.js
+++ src/components/Sponsors/Sponsorcard/Sponsorcard.js
@@ -1,16 +1,55 @@
 import React from "react";
 import "./Sponsorcard.css";
-import logo from "../../../assets/images/discord.png";
+import sapphirestone from "../../../assets/images/sapphirestone.png";
+import emeraldstone from "../../../assets/images/emeraldstone.png";
+import rubystone from "../../../assets/images/rubystone.png";
+import diamondstone from "../../../assets/images/diamondstone.png";
 
-function Sponsorcard({ image, name }) {
-  return (
-    <div className="sponsor_card_wrapper">
-      <img src={image} alt="" />
-      <div className="sponsor_card_name_wrapper">
-        <h3>{name}</h3>
+function Sponsorcard({ image, name, tier }) {
+  if (tier === "Emerald") {
+    return (
+      <div className="sponsor_card_wrapper emerald_tier">
+        <img src={image} alt="" className="sponsor_logo" />
+        {/* <div className="sponsor_card_name_wrapper">
+          <h3>{name}</h3>
+        </div> */}
+        <img src={emeraldstone} alt="" className="sponsor_card_stone" />
       </div>
-    </div>
-  );
+    );
+  }
+  if (tier === "Ruby") {
+    return (
+      <div className="sponsor_card_wrapper ruby_tier">
+        <img src={image} alt="" className="sponsor_logo" />
+        {/* <div className="sponsor_card_name_wrapper">
+          <h3>{name}</h3>
+        </div> */}
+        <img src={rubystone} alt="" className="sponsor_card_stone" />
+      </div>
+    );
+  }
+  if (tier === "Sapphire") {
+    return (
+      <div className="sponsor_card_wrapper sapphire_tier">
+        <img src={image} alt="" className="sponsor_logo" />
+        {/* <div className="sponsor_card_name_wrapper">
+          <h3>{name}</h3>
+        </div> */}
+        <img src={sapphirestone} alt="" className="sponsor_card_stone" />
+      </div>
+    );
+  }
+  if (tier === "Diamond") {
+    return (
+      <div className="sponsor_card_wrapper diamond_tier">
+        <img src={image} alt="" className="sponsor_logo" />
+        {/* <div className="sponsor_card_name_wrapper">
+          <h3>{name}</h3>
+        </div> */}
+        <img src={diamondstone} alt="" className="sponsor_card_stone" />
+      </div>
+    );
+  }
 }
 
 export default Sponsorcard;
--- src/components/Sponsors/Sponsorsection/Sponsorsection.js
+++ src/components/Sponsors/Sponsorsection/Sponsorsection.js
@@ -5,10 +5,10 @@ import "./Sponsorsection.css";
 function Sponsorsection({ tier, sponsorlist, color }) {
   return (
     <div className="sponsor_section_wrapper">
-      <h2 style={{ color: color }}>{tier}</h2>
+      {/* <h2 style={{ color: color }}>{tier}</h2> */}
       <div className="sponsor_section_inner_wrapper">
         {sponsorlist.map((sponsors) => (
-          <Sponsorcard image={sponsors.img} name={sponsors.name} />
+          <Sponsorcard image={sponsors.img} name={sponsors.name} tier={tier} />
         ))}
       </div>
     </div>
