4ba48629cd35095fb78fb867d515c10443d40315
pranav madhu <pranavmadhu73@gmail.com>
2022-10-13 16:19:37 +0530

    added navigation to home from dashboard

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Logo.css";
+import { useNavigate } from "react-router-dom";
 
 function Logo({ logo }) {
+  const navigate = useNavigate();
+  function jumpHome() {
+    navigate("/");
+  }
   return (
-    <div className="dashboard_logo_wrapper">
+    <div className="dashboard_logo_wrapper" onClick={jumpHome}>
       <img src={logo} alt="" />
     </div>
   );
