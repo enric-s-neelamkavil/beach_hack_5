b99af6e1b7d1214288986f9b2b4d515762750139
pranav madhu <pranavmadhu73@gmail.com>
2022-10-13 16:13:14 +0530

    changed the header fixed image display in scroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Logo/Logo";
 import "./Header.css";
 import Button from "../Button/Button";
@@ -6,9 +6,24 @@ import logo from "../../assets/images/logowhite.png";
 import bg from "../../assets/images/bg.svg";
 
 function Header({ anchorlinks }) {
+  const [open, setOpen] = useState(true);
+  const changeopen = () => {
+    if (window.scrollY >= 80) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+  window.addEventListener("scroll", changeopen);
   return (
     <header className="header">
-      <img src={bg} alt="" />
+      <img
+        src={bg}
+        alt=""
+        className={
+          "header_fixed_image " + (!open ? "header_fixed_image_active" : "")
+        }
+      />
       <Logo logo={logo} />
       <nav className="header_navbar">
         <ul>
