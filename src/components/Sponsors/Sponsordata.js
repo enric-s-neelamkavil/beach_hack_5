54bfe16020e84e799bafce924a6e8eaffda12ccf
pranav madhu <pranavmadhu73@gmail.com>
2022-10-29 11:17:42 +0530

    compressed sponsor images in firebase

--- src/components/Sponsors/Sponsordata.js
+++ src/components/Sponsors/Sponsordata.js
@@ -4,7 +4,7 @@ export const sponsor = [
     color: "red",
     sponsorlist: [
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fggnation.png?alt=media&token=08448b16-f71b-4c6f-9dbb-7c7cc27988b8`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fggnation.png?alt=media&token=621f6c42-de10-4d42-a537-78c4ec115c00`,
         url: `https://ggnation.com/`,
       },
     ],
@@ -14,15 +14,15 @@ export const sponsor = [
     color: "#E5E4E2",
     sponsorlist: [
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fneurahire.png?alt=media&token=1836ba0d-6f97-44e8-a2c4-92f1214450db`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fneurahire.png?alt=media&token=c964c8d9-1bdc-4f53-a1a8-e7fbdde2d25f`,
         url: `https://www.neurahire.com/`,
       },
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fdevfoliospo.png?alt=media&token=ac5a31f8-1e1e-4e53-b3c1-639886cd7c4f`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fdevfoliospo.png?alt=media&token=90acc88e-5ada-4405-9714-b949156e0baa`,
         url: `https://devfolio.co`,
       },
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fpolygon.png?alt=media&token=294f1b3d-1187-4bfe-b2fb-ca5da9c78e05`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fpolygon.png?alt=media&token=8e8b50ab-5ffe-46d3-8461-a09f40027994`,
         url: `https://polygon.technology/`,
       },
     ],
@@ -32,22 +32,22 @@ export const sponsor = [
     color: "#FFD700",
     sponsorlist: [
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fiedc.png?alt=media&token=651cb812-07f3-4b0b-94c9-5cb5e7f5b1f0`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fiedc.png?alt=media&token=b81cf48c-5ba0-4c94-b780-a3e29ed89b94`,
         url: `https://iedc.cce.edu.in/`,
       },
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fieee.png?alt=media&token=aedd2d26-6f51-441f-8146-a3dd118ad660`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fieee.png?alt=media&token=247b3f35-406b-46c6-8c48-6e283f1c80f1`,
       },
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fsolana.png?alt=media&token=2fd0e912-ab0b-41f5-a4e9-6e2c529661b1`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fsolana.png?alt=media&token=413e11d9-fb89-4ba0-a2b9-08a570d942e1`,
         url: `https://solana.com `,
       },
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Ffilecoin.png?alt=media&token=348b457c-af0b-4d53-a6ca-e77515d7be1a`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Ffilecoin.png?alt=media&token=e678ba97-f40f-4fd9-8e3c-0d521142fbd6`,
         url: `https://filecoin.io `,
       },
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Freplit.png?alt=media&token=e0180e42-2d25-4474-b179-b58e59803f8f`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Freplit.png?alt=media&token=0b436af4-bdfb-46eb-a0da-6343db1e78b8`,
         url: `https://replit.com `,
       },
     ],
@@ -57,7 +57,7 @@ export const sponsor = [
     color: "#C0C0C0",
     sponsorlist: [
       {
-        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Ftinkerhub.png?alt=media&token=482ebb7b-afba-41ba-a6bd-008e7f9ca983`,
+        img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Ftinkerhub.png?alt=media&token=3bb48d51-e7db-4303-871c-f2f93b3820b0`,
         url: `https://tinkerhub.org/`,
       },
     ],
