ae9589173fc552100e15668d2225404e7b536e4a
pranav madhu <pranavmadhu73@gmail.com>
2022-11-20 16:24:24 +0530

    added theme

--- src/components/Association/Associationdata.js
+++ src/components/Association/Associationdata.js
@@ -1,25 +1,22 @@
 export const association = [
   {
     img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fcode.png?alt=media&token=dcaf6621-0f1f-4a2f-9ebd-f77351cf2365`,
-    // url: `https://iedc.cce.edu.in/`,
   },
   {
     img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fiedc.png?alt=media&token=b81cf48c-5ba0-4c94-b780-a3e29ed89b94`,
     url: `https://iedc.cce.edu.in/`,
   },
-  // {
-  //   img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2FGDSC%20(lIGHT).png?alt=media&token=691ecacf-0ea2-4f5d-8c77-8d6e4e945f0c`,
-  // },
   {
     img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Ftinkerhu.png?alt=media&token=169105fb-1f63-45e3-acbc-dfdfb4f168ac`,
-    url: `https://tinkerhub.org/`,
+  },
+  // {
+  //   img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fieee%20(1).png?alt=media&token=2dc4193c-fff4-497e-bea9-f416215b6d5c`,
+  // },
+  {
+    img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2FCCE%20SB%20White%20logo%20(1).png?alt=media&token=b8edcc1b-b1cc-4c96-a673-9d5cfe07fdb3`,
   },
   {
-    img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fieee%20(1).png?alt=media&token=2dc4193c-fff4-497e-bea9-f416215b6d5c`,
-    // url: `https://tinkerhub.org
+    img: `https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fcsii.png?alt=media&token=54730c16-8a42-440a-8027-22681a9dd4f1`,
+    url: ``,
   },
-  {
-    img:`https://firebasestorage.googleapis.com/v0/b/beachhack5.appspot.com/o/sponsors%2Fcsii.png?alt=media&token=54730c16-8a42-440a-8027-22681a9dd4f1`,
-    url:``
-  }
 ];
--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -8,9 +8,15 @@ function Theme() {
       <div className="theme_inner_wrapper">
         <div className="theme_title_wrapper">
           <h2>Theme : </h2>
-          <p>This is a test theme which is pretty long to test.</p>
+          <p className="themeTitle">MARINE AND FISHERIES</p>
+          <span className="absoluteSpan">*The above mentioned theme is only for the first selection process.</span>
+
         </div>
         <div className="theme_problem_wrapper">
+          <div className="problemAbsoluteDiv">
+            <span>Problem statements will be released on 27th November 2022</span>
+            <span>Participants can also opt for their own problem statements</span>
+          </div>
           <h2>Problem Statements : </h2>
           <ul className="problem_list">
             {questions.map((problems,index) => (
