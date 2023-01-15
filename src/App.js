1c09d3e5332e91485175af350fbeac95b847f0e1
pranav madhu <pranavmadhu73@gmail.com>
2023-01-15 08:36:52 +0530

    added essential manifest.json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState,lazy } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Schedulepage from "./pages/Schedulepage/Schedulepage";
-import Leaderboardpage from "./pages/Leaderboard/Leaderboardpage";
-import Sponsorspage from "./pages/Sponsorspage/Sponsorspage";
-import Aboutpage from "./pages/Aboutpage/Aboutpage";
-import Activesoonpage from "./pages/Activesoonpage/Activesoonpage";
-import Bottomnav from "./components/Bottomnav/Bottomnav";
-import Header from "./components/Header/Header";
+// import Home from "./pages/Home/Home";
+// import Dashboard from "./pages/Dashboard/Dashboard";
+// import Schedulepage from "./pages/Schedulepage/Schedulepage";
+// import Sponsorspage from "./pages/Sponsorspage/Sponsorspage";
+// import Aboutpage from "./pages/Aboutpage/Aboutpage";
+// import Activesoonpage from "./pages/Activesoonpage/Activesoonpage";
+// import Bottomnav from "./components/Bottomnav/Bottomnav";
+// import Header from "./components/Header/Header";
+// import Leaderboardpage from "./pages/Leaderboard/Leaderboardpage";
 import Aos from "aos";
 import "aos/dist/aos.css";
-
+import Loading from "./Loading";
+const Home = lazy(() => import("./pages/Home/Home"));
+const Dashboard = lazy(() => import("./pages/Dashboard/Dashboard"));
+const Schedulepage = lazy(() => import("./pages/Schedulepage/Schedulepage"));
+const Leaderboardpage = lazy(() =>
+  import("./pages/Leaderboard/Leaderboardpage")
+);
+const Sponsorspage = lazy(() => import("./pages/Sponsorspage/Sponsorspage"));
+const Aboutpage = lazy(() => import("./pages/Aboutpage/Aboutpage"));
+const Bottomnav = lazy(() => import("./components/Bottomnav/Bottomnav"));
+const Header = lazy(() => import("./components/Header/Header"));
 
 function App() {
   useEffect(() => {
@@ -21,19 +31,27 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header
-          anchorlinks={["home", "schedule", "leaderboard", "sponsors", "about"]}
-        />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Dashboard />} />
-          <Route path="/schedule" element={<Schedulepage />} />
-          <Route path="/leaderboard" element={<Leaderboardpage />} />
-          <Route path="/sponsors" element={<Sponsorspage />} />
-          <Route path="/about" element={<Aboutpage />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-        <Bottomnav />
+        <Suspense fallback={<Loading />}>
+          <Header
+            anchorlinks={[
+              "home",
+              "schedule",
+              "leaderboard",
+              "sponsors",
+              "about",
+            ]}
+          />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Dashboard />} />
+            <Route path="/schedule" element={<Schedulepage />} />
+            <Route path="/leaderboard" element={<Leaderboardpage />} />
+            <Route path="/sponsors" element={<Sponsorspage />} />
+            <Route path="/about" element={<Aboutpage />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+          <Bottomnav />
+        </Suspense>
       </BrowserRouter>
     </>
   );
